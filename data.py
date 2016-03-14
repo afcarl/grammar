05c6ea9b6b4cca74a5aa81b3f702c1e24ab119db
Ofir Nachum <ofir@Ofirs-MacBook-Pro.local>
2016-03-13 17:42:29 -0700

    data preparation methods

---
+++ data.py
@@ -0,0 +1,106 @@
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+import os
+import subprocess
+
+PARSER_DIR = '~/berkeleyparser'
+
+
+def get_book():
+    """Downloads 'Moby Dick'"""
+    subprocess.check_ouput(
+        ['wget', 'http://www.gutenberg.org/cache/epub/2701/pg2701.txt'])
+
+
+def read_paragraphs(book_file, limit=1000):
+    """Reads file and returns list of paragraphs.
+
+    Assumes paragraphs are delimited by blank lines.
+
+    """
+    paragraphs = []
+    current_str = ''
+    with open(book_file, 'r') as f:
+        for line in f:
+            stripped = line.strip()
+            # check for end of paragraph
+            if '' == stripped and current_str.strip():
+                paragraphs.append(current_str.strip())
+                current_str = ''
+            if stripped:
+                current_str += stripped + ' '
+            if len(paragraphs) >= limit:
+                break
+        else:
+            # add last paragraph
+            if current_str:
+                paragraphs.append(current_str.strip())
+    return paragraphs
+
+
+def compute_grammar(sentences):
+    """Uses Berkeley Parser to compute grammar trees on list of sentences."""
+    if not sentences:
+        return []
+    my_parser = os.path.join(PARSER_DIR, 'BerkeleyParser-1.7.jar')
+    my_grammar = os.path.join(PARSER_DIR, 'eng_sm6.gr')
+    p = subprocess.Popen(
+        ' '.join(['java', '-jar', my_parser, '-gr', my_grammar]),
+        stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=None,
+        shell=True)
+
+    stdout = p.communicate(input='\n'.join(sentences))[0]
+    lines = stdout.split('\n')
+    if len(lines) != len(sentences):  # last line is empty leftover
+        assert len(lines) == 1 + len(sentences)
+        lines = lines[:-1]
+    return lines
+
+
+def extract_sentence_grammars(book_file, num_paragraphs=1000, sent_len_limit=150):
+    """Extracts sentences and their grammars from book file."""
+    paragraphs = read_paragraphs(book_file, limit=num_paragraphs)
+    # attempt to find first paragraph of book
+    for first_idx, paragraph in enumerate(paragraphs):
+        if 'Call me Ishmael.' in paragraph:
+            break
+    else:
+        first_idx = 0
+    paragraphs = paragraphs[first_idx:]
+
+    # tokenize sentences
+    sentences = []
+    for paragraph in paragraphs:
+        para_sentences = sent_tokenize(paragraph)
+        sentences.extend([sent for sent in para_sentences
+                          if sent.strip() and len(sent) <= sent_len_limit])
+
+    grammars = compute_grammar(sentences)
+    return zip(sentences, grammars)
+
+
+def tokenize_sentence(s, lower=True):
+    """Tokenize a sentence into words."""
+    if lower:
+        s = s.lower()
+    return word_tokenize(s)
+
+
+def tokenize_grammar(g):
+    """Tokenize linear parse tree g."""
+    g = g.replace(')', ' )')
+    init_tokens = g.split()
+    out_tokens = []
+    path = []
+    for tok in init_tokens:
+        if ')' in tok:
+            assert tok == ')'
+            assert path
+            complement = path.pop()
+            out_tokens.append(complement.replace('(', ')'))
+        elif '(' in tok:
+            path.append(tok)
+            out_tokens.append(tok)
+        else:  # leaf node is just a word
+            out_tokens.append('XX')
+    return out_tokens
